b1130845d51aa510bbc3c881974dec40e7eea4f9
Thomas-Crudden <s00141926@mail.itsligo.ie>
2016-03-23 19:15:47 +0000

    commit

---
+++ S00141926_RAD302_Assignment/S00141926_RAD302_Assignment/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="S00141926_RAD302_Assignment.MvcApplication" Language="C#" %>
---
+++ S00141926_RAD302_Assignment/S00141926_RAD302_Assignment/Models/Characters.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+
+namespace S00141926_RAD302_Assignment.Models
+{
+    public class Characters
+    {
+        public int ID { get; set;}
+        public string Name { get; set; }
+        public string Actor { get; set; }
+
+    }
+
+    public class CharactersDBContext : DbContext
+    {
+        public DbSet<Characters> Characters { get; set; }
+    }
+}
